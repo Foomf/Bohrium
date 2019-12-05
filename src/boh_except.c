b4c06ccec94a1f96f0fedb0dcc991a0344a649e3
Foomf <foomf@protonmail.com>
2019-12-04 22:18:20 -0800

    Fix strncpy calls

--- src/boh_except.c
+++ src/boh_except.c
@@ -7,8 +7,8 @@
 
 void boh_throw_except(CEXCEPTION_T id, char* file, int line_num, char* msg) {
     boh_error_line_number = line_num;
-    strncpy(boh_error_file, file, boh_filename_buffer_size - 1);
-    strncpy(boh_error_message, msg, boh_error_buffer_size - 1);
+    strncpy(boh_error_file, file, boh_filename_buffer_size);
+    strncpy(boh_error_message, msg, boh_error_buffer_size);
     boh_last_error_id = id;
 
     ZF_LOGI("Message: %s", msg);
