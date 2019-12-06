780ebfd6dc54b77de7a4a4fd04f4ec5c2e898861
Foomf <foomf@protonmail.com>
2019-12-06 01:04:40 -0800

    Document guid

--- include/guid.h
+++ include/guid.h
@@ -9,6 +9,18 @@
 typedef GUID BOH_GUID;
 #endif
 
+/**
+ * @brief Generates a guid.
+ * 
+ * @param guid The guid to create.
+ */
 void make_guid(BOH_GUID* guid);
 
+/**
+ * @brief Converts a guid to a string.
+ * 
+ * @param self The guid to convert to string.
+ * @return sds The string representing the guid. Ownership is passed
+ *             to the caller, who must call sdsfree().
+ */
 sds guid_to_string(BOH_GUID* self);
